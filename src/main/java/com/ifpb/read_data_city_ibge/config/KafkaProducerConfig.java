8ab5f2a5df990eb6ba6cfc0f076f5275650963b7
LucianoCarvalhoF <luciano_carvalho@outlook.com.br>
2025-12-13 20:51:32 -0600

    Update Kafka configs

--- src/main/java/com/ifpb/read_data_city_ibge/config/KafkaProducerConfig.java
+++ src/main/java/com/ifpb/read_data_city_ibge/config/KafkaProducerConfig.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tools.jackson.databind.ser.jdk.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,15 +15,19 @@ import java.util.Map;
 @SuppressWarnings("unused")
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.producer.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
 
     @Bean
     public KafkaProducer<String, String> kafkaProducer() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
 
         props.put("security.protocol", "SSL");
         props.put("ssl.endpoint.identification.algorithm", "https");
